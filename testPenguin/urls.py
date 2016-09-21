9032b756ad8f59d435daa3aaa254ecf7cbfcb96f
Saad Khan <saad.khan02@gmail.com>
2016-09-20 22:09:24 -0400

    Add url mapping for every single test case and modify details page.

--- testPenguin/urls.py
+++ testPenguin/urls.py
@@ -2,6 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^testCases/(?P<case_name_slug>[\w\-]+)/$', views.testCaseDetails,
+        name='testCaseDetails'),
+    url(r'^testCases/(?P<case_name_slug>[\w\-]+)/modifyCase/$',
+        views.modifyCase, name='modifyCase'),
     url(r'^testSuites/(?P<suite_name_slug>[\w\-]+)/$', views.testSuiteDetails,
         name='testSuiteDetails'),
     url(r'^testCases/$', views.testCases, name='testCases'),
