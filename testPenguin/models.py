cbdab2c64c4232a4ce1ffd73ce2fe36d32cf3c89
Saad Khan <saad.khan02@gmail.com>
2016-10-06 23:21:30 -0400

    Make changes to Step table to suite business needs:  * Remove unique classifier from step_name  * Comment out step_created and step_modified. These will be used for audit    puposes, probably in phase 2.  * Remove unique classifier from step_slug  * Give more meaningful names to arguments: locator type, locator and value.

--- testPenguin/models.py
+++ testPenguin/models.py
@@ -46,18 +46,21 @@ class Case(models.Model):
 
 class Step(models.Model):
     case = models.ForeignKey(Case)
-    step_name = models.CharField(max_length=128, unique=True)
-    step_created = models.DateTimeField("Date created")
-    step_modified = models.DateTimeField("Date modified")
-    step_slug = models.SlugField(unique=True)
+    step_name = models.CharField(max_length=128)
+#    We're not going to use step_created and step_modified till at least phase 2
+#    step_created = models.DateTimeField("Date created")
+#    step_modified = models.DateTimeField("Date modified")
+    step_slug = models.SlugField()
+    step_order = models.IntegerField()
     always_run = models.CharField(max_length=5, default="false")
     action = models.CharField(max_length=128)
-    arg1 = models.CharField(max_length=128)
-    arg2 = models.CharField(max_length=128)
-    arg3 = models.CharField(max_length=128)
+    locator_type = models.CharField(max_length=128, blank=True)
+    locator = models.CharField(max_length=128, blank=True)
+    value = models.CharField(max_length=128, blank=True)
 
     def save(self, *args, **kwargs):
-        self.step_slug = slugify(step_name)
+        self.step_slug = slugify(self.step_name)
+#        self.step_modified = datetime.datetime.now()
         super(Step, self).save(*args, **kwargs)
 
     def __str__(self):
